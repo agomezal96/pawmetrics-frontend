75fa551a5e8ca3bdbed6a0a9281b1054b6a8675b
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-25 16:20:18 +0100

    Add children prop to Dashboard and add it to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import EarningsCard from "./components/cards/EarningsCard"
+import Dashboard from "./components/Dashboard"
 import Header from "./components/Header"
+
+const mockData = {
+  earnings: {
+    past_earnings: 1200,
+    future_earnings:250,
+  }
+}
 
 function App() {
 
   return (
     <>
       <Header />
+      <Dashboard>
+        <EarningsCard earnings={mockData.earnings}/>
+      </Dashboard>
     </>
   )
 }
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,15 @@
+import type { ReactNode } from 'react';
 import './Dashboard.css';
 
-export default function Dashboard() {
+interface DashboardProps {
+  children: ReactNode
+}
+
+export default function Dashboard({children}:DashboardProps) {
     
   return (
     <div className="bento-grid">
-      
+      {children}
     </div>
   );
 }
