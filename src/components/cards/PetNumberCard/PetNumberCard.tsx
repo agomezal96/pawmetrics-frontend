44c575b6bb547c1ee52e7a41a55d5e3eb50f560f
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-25 17:39:00 +0100

    Create PetNumberCard

---
+++ src/components/cards/PetNumberCard/PetNumberCard.tsx
@@ -0,0 +1,13 @@
+import styles from './PetNumberCard.module.css'
+
+interface petNumberCardProps(){
+    
+}
+
+export default function PetNumberCard() {
+    return (
+    <article className='card'>
+        
+    </article>
+    )
+}
