4fdcadcee06e34105940916ec923b569163fef2b
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-25 16:44:23 +0100

    Fix EarningsCard styles

--- src/components/cards/EarningsCard/EarningsCard.tsx
+++ src/components/cards/EarningsCard/EarningsCard.tsx
@@ -12,11 +12,15 @@ export default function EarningsCard({ earnings }: EarningsCardProps) {
       <section className={styles['box-content']}>
         <div className={styles['earning-box']}>
           <h3>Current</h3>
-          <p className={styles['current-earnings']}>{earnings.past_earnings}$</p>
+          <p className={styles['current-earnings']}>
+            {earnings.past_earnings}$
+          </p>
         </div>
         <div className={styles['earning-box']}>
           <h3>Pending</h3>
-          <p className={styles['pending-earnings']}>{earnings.future_earnings}$</p>
+          <p className={styles['pending-earnings']}>
+            {earnings.future_earnings}$
+          </p>
         </div>
       </section>
       <p className="time">This year</p>
