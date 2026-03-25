826354a7c12d5b350be035c33724934cf47aa6fa
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-25 17:54:37 +0100

    Fix PetNumberCard

--- src/components/cards/PetNumberCard/PetNumberCard.tsx
+++ src/components/cards/PetNumberCard/PetNumberCard.tsx
@@ -1,13 +1,22 @@
-import styles from './PetNumberCard.module.css'
+import styles from './PetNumberCard.module.css';
+import type { ReactNode } from 'react';
 
-interface petNumberCardProps(){
-    
+interface petNumberCardProps {
+  numberOfPets: number;
+  icon: ReactNode;
+  label?: string;
 }
 
-export default function PetNumberCard() {
-    return (
-    <article className='card'>
-        
+export default function PetNumberCard({
+  numberOfPets,
+  icon,
+  label,
+}: petNumberCardProps) {
+  return (
+    <article className={`card ${styles['pet-number-card']}`}>
+      {icon}
+      <p className={styles['number-of-pets']}>{numberOfPets}</p>
+      <p>{label}</p>
     </article>
-    )
+  );
 }
