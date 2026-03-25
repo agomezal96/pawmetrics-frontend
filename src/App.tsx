804f1518d2d2b0abb463831d87f957eb8b7193a0
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-25 16:45:09 +0100

    Load components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import EarningsCard from "./components/cards/EarningsCard"
-import Dashboard from "./components/Dashboard"
-import Header from "./components/Header"
+import EarningsCard from './components/cards/EarningsCard';
+import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import MainLayout from './layouts/MainLayout';
 
 const mockData = {
   earnings: {
     past_earnings: 1200,
-    future_earnings:250,
-  }
-}
+    future_earnings: 250,
+  },
+};
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Dashboard>
-        <EarningsCard earnings={mockData.earnings}/>
-      </Dashboard>
+      <MainLayout>
+        <Header />
+        <Dashboard>
+          <EarningsCard earnings={mockData.earnings} />
+        </Dashboard>
+      </MainLayout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
