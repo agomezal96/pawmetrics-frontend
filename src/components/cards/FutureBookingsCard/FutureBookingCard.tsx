bcb41c072a1934a038ee4335b3b095ec26e8bb4a
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-26 17:17:01 +0100

    Add styles to FutureBookingCard.css

--- src/components/cards/FutureBookingsCard/FutureBookingCard.tsx
+++ src/components/cards/FutureBookingsCard/FutureBookingCard.tsx
@@ -10,20 +10,19 @@ export default function FutureBookingCard({ booking }: FutureBookingProps) {
   return (
     <article className="card">
       <p className={styles.date}>{booking.start_date}</p>
-      <div className={styles['future-booking-card']}>
-        <div>
-          <div className={styles['icon-and-name']}>
-            <span>
-              {booking.pet_species === 'Dog' ? (
-                <Dog size={24} />
-              ) : (
-                <Cat size={24} />
-              )}
-            </span>
-            <div>
-              <p className={styles['pet-name']}>{booking.pet_name}</p>
-              <p className={styles['service']}>{booking.service}</p>
-            </div>
+      <div className={styles['future-booking-content']}>
+        <div className={styles['icon-and-name']}>
+          <div className={styles['icon-wrapper']}>
+            {booking.pet_species === 'Dog' ? (
+              <Dog size={26} weight="bold" />
+            ) : (
+              <Cat size={26} weight="bold" />
+            )}
+          </div>
+
+          <div className={styles['pet-details']}>
+            <p className={styles['pet-name']}>{booking.pet_name}</p>
+            <p className={styles['service']}>{booking.service}</p>
           </div>
         </div>
       </div>
