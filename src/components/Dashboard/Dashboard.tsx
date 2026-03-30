7d6f984464683b4eb7b644ddfb1bbff4c0637506
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-03-30 16:36:40 +0200

    Rename LoadingModule to LoadingDashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,7 +7,7 @@ import ReviewModule from '../modules/ReviewModule/ReviewModule';
 
 import useGetRequest from '../../hooks/useGetRequest';
 import type { DashboardMetrics } from '../../types/dashboard';
-import LoadingModule from '../modules/LoadingModule';
+import LoadingDashboard from '../LoadingDashboard';
 
 export default function Dashboard() {
   const baseUrl = import.meta.env.VITE_API_METRICS_URL;
@@ -18,7 +18,7 @@ export default function Dashboard() {
     error,
   } = useGetRequest<DashboardMetrics>(baseUrl);
 
-  if (isLoading) return <LoadingModule />;
+  if (isLoading) return <LoadingDashboard />;
   if (error) return <p>Error: {error}</p>;
   if (!dashboardMetrics) return null;
 
