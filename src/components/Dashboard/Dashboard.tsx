197c842d97dfd8a4e6309c6dcc2f14238e633a12
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-04-01 09:29:07 +0200

    Add the PeriodSelector to Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,31 +6,51 @@ import FutureBookingsModule from '../modules/FutureBookingsModule';
 import ReviewModule from '../modules/ReviewModule/ReviewModule';
 
 import useGetRequest from '../../hooks/useGetRequest';
-import type { DashboardMetrics } from '../../types/dashboard';
+import {
+  type DashboardPeriod,
+  type DashboardMetrics,
+} from '../../types/dashboard';
 import LoadingDashboard from '../LoadingDashboard';
 import ErrorDashboard from '../ErrorDashboard/ErrorDashboard';
+import { useMemo, useState } from 'react';
+import PeriodSelector from '../PeriodSelector';
 
 export default function Dashboard() {
   const baseUrl = import.meta.env.VITE_API_METRICS_URL;
+  const [period, setPeriod] = useState<DashboardPeriod>('this_year');
+  const queryParams = useMemo(
+    () => ({ period }), // The object I want that react remembers
+    [period], //only change it if the variable 'period' changes.
+  );
+
+  function handlePeriodChange(period) {
+    setPeriod(period);
+  }
 
   const {
-    requestData: dashboardMetrics,
+    requestData: metrics,
     isLoading,
     error,
-  } = useGetRequest<DashboardMetrics>(baseUrl);
+  } = useGetRequest<DashboardMetrics>(baseUrl, queryParams);
 
+  console.log(metrics);
   if (isLoading) return <LoadingDashboard />;
   if (error) return <ErrorDashboard message={error} />;
-  if (!dashboardMetrics) return null;
+  if (!metrics) return null;
 
-  const { bookings, earnings, pets, reviews } = dashboardMetrics;
+  const { bookings, earnings, pets, reviews } = metrics;
 
   return (
     <div className={styles['dashboard-container']}>
-      <p className={styles['title-welcome']}>Hello, Andrea</p>
+      <div className={styles['title-and-selector']}>
+        <p className={styles['title-welcome']}>Hello, Andrea</p>
+        <PeriodSelector
+          currentPeriod={period}
+          onPeriodChange={handlePeriodChange}
+        />
+      </div>
       <p className={styles['p-welcome']}>
-        Here you can easily track your earnings, manage upcoming bookings, and
-        keep an eye on your pet stats.
+        Your stats for {period.replace('_', ' ')}
       </p>
       <div className={styles['bento-grid']}>
         <EarningsModule earnings={earnings} />
