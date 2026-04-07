052fa7f40c6b2ed41848c71fd389a2fbd14a5b92
Andrea Gomez Alvarez <andreagoal96@gmail.com>
2026-04-07 10:15:36 +0200

    Refactor Header component layout and styles for improved structure and consistency

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,20 @@
 import './Header.css';
-import { PawPrint, User } from 'phosphor-react';
+import { PawPrint } from 'phosphor-react';
+import avatar from '../../assets/images/avatar.png';
 
 export default function Header() {
   return (
     <header>
       <nav className="box">
         <div className="logo-container">
-          <span className="logo"><PawPrint color="var(--brand)" weight='fill' size={46}/></span>
+          <span className="logo">
+            <PawPrint color="var(--brand)" weight="fill" size={46} />
+          </span>
         </div>
-        
+
         <div className="user-section">
-          <div className='avatar-container'>
-            <User size={32} weight="regular" />
+          <div className="avatar-container">
+            <img src={avatar} alt="Avatar image" />
           </div>
         </div>
       </nav>
